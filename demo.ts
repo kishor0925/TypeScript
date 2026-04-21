576220faf7038c3a46039cabb9a107356cd599e3
kishor0925 <kishor25092004@gmail.com>
2026-04-21 12:19:23 +0530

    TypeScript Basics

---
+++ demo.ts
@@ -0,0 +1,21 @@
+/*
+let input : string | null = prompt();
+
+if(input !== null){
+
+    let age : number = Number(input);
+
+    if(age >= 18){
+        console.log('eligible for vote');
+    }
+    else{
+        console.log('not eligible for vote');
+    }
+}
+else{
+    console.log("No input provided")
+}
+*/
+
+let numbers : number[] = [1,2,3,4,5];
+console.log(numbers[3]); 
